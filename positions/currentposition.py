5bd393b3e01c418fb2b7bb70827fbcf908da1e46
MorceauDeRouille <s.gibard@gmail.com>
2023-12-03 19:03:35 +0100

    Create mother class Position

--- positions/currentposition.py
+++ positions/currentposition.py
@@ -1,11 +1,11 @@
 import pandas as pd
+from position import Position
 
 
-class CurrentPosition:
+class CurrentPosition(Position):
 
     def __init__(self, row, col):
-        self.row = row
-        self.col = col
+        super().__init__(row, col)
 
     def evaluate_action_according_to_current_position(self, grid: pd.DataFrame, player: int) -> str:
         if self.__is_current_position_occupied_by_player(grid, player):
---
+++ positions/position.py
@@ -0,0 +1,5 @@
+class Position:
+
+    def __init__(self, row, col):
+        self._row = row
+        self._col = col
